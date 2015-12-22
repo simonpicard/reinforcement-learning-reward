3cabd2f7ce63dce4daa960f212b19875b8235450
jbastogn <jbastogn@ulb.ac.be>
2015-12-22 19:41:43 +0100

    maj

--- rsMARL.py
+++ rsMARL.py
@@ -1,11 +1,15 @@
 from random import random, choice
 
 class rsMARL:
-    def __init__(self, filename):
+    def __init__(self, filename, epsilon, gamma, alpha):
         self.parse(filename)
 
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.alpha = alpha
+
         self.moves = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        self.goalReached = [False]*len(self.flags)
+        
         '''
 
         self.canMove(0, 0)
@@ -62,7 +66,7 @@ class rsMARL:
                 self.world[y][x] = content[y][x]
         #print(self.world)
         del content[:13]
-        print(content)
+        #print(content)
         
         size = int(content[0].strip("\n"))
         del content[0]
@@ -77,7 +81,7 @@ class rsMARL:
         for i in range(size):
             self.roomsNames.append(content[0].strip("\n"))
             del content[0]
-        print(self.flagsNames, self.roomsNames)
+        #print(self.flagsNames, self.roomsNames)
             
         size = int(content[0].strip("\n"))
         del content[0]
@@ -129,14 +133,23 @@ class rsMARL:
         availableMoves = []
         potentialPoses = [[x+1, y], [x-1, y], [x, y+1], [x, y-1]]
         potentialMoves = range(4)
+
         for i in range(len(potentialPoses)):
             if potentialPoses[i][0] >= 0 and potentialPoses[i][1] >= 0 and potentialPoses[i][0] < self.size[0] and potentialPoses[i][1] < self.size[1]:
+
                 if self.isInSameRoom([x, y], potentialPoses[i]):
                     availableMoves.append(potentialMoves[i])
                     availablePoses.append(potentialPoses[i])
-                if self.hasDoor([x, y], potentialPoses[i]):
+                elif self.hasDoor([x, y], potentialPoses[i]):
                     availableMoves.append(potentialMoves[i])
                     availablePoses.append(potentialPoses[i])
+
+        if self.agent1 in availablePoses and self.agent1!=self.goal:
+            availableMoves.pop(availablePoses.index(self.agent1))
+            availablePoses.remove(self.agent1)
+        if self.agent2 in availablePoses and self.agent2!=self.goal:
+            availableMoves.pop(availablePoses.index(self.agent2))
+            availablePoses.remove(self.agent2)
         return availablePoses, availableMoves
 
     def hasDoor(self, coord1, coord2):
@@ -150,44 +163,63 @@ class rsMARL:
         return self.agent1 == self.goal and self.agent2 == self.goal
 
 
-    def run(self):
-        self.Qas = [[0, 0, 0, 0] for i in range(self.size[0])] for j in range(self.size[1])]
+    def reset(self):
         self.agent1 = self.start[0]
         self.agent2 = self.start[1]
+        self.goalReached = [False]*len(self.flags)
 
-        while not self.isDone() :
-            action1 = self.chooseAction(self.agent1[0], self.agent1[1])
-            nextPos1 = self.getNextPos(x, y, action1)
-            reward1 = self.getReward(nextPos1)
-            self.updateQ(reward1, nextPos1, self.agent1, action1)
-            self.agent1 = nextPos1
+    def run(self):
+        Qas1 = [[[0, 0, 0, 0] for i in range(self.size[1])] for j in range(self.size[0])]
+        Qas2 = [[[0, 0, 0, 0] for i in range(self.size[1])] for j in range(self.size[0])]
+
+        for run in range(1000):
+            self.reset()
+            step = 0
+            while not self.isDone() :
+                step += 1
+                if self.agent1 != self.goal:
+                    action1 = self.chooseAction(self.agent1[0], self.agent1[1], Qas1)
+                    nextPos1 = self.getNextPos(self.agent1[0], self.agent1[1], action1)
+                    reward1 = self.getReward(nextPos1)
+                    Qas1 = self.updateQ(reward1, nextPos1, self.agent1, action1, Qas1)
+                    self.agent1 = nextPos1
+                    self.checkFlags(self.agent1)
+
+                if self.agent2 != self.goal:
+                    action2 = self.chooseAction(self.agent2[0], self.agent2[1], Qas2)
+                    nextPos2 = self.getNextPos(self.agent2[0], self.agent2[1], action2)
+                    reward2 = self.getReward(nextPos2)
+                    Qas2 = self.updateQ(reward2, nextPos2, self.agent2, action2, Qas2)
+                    self.agent2 = nextPos2
+                    self.checkFlags(self.agent2)
+            print (step, self.goalReached.count(True))
 
 
     def probability(self, p):
         return random() < p
 
-    def chooseAction(self, x, y):
-        availablePoses, availableMoves = canMove(x, y)
-        if probability(self.epsilon):
+    def chooseAction(self, x, y, Qas):
+        availablePoses, availableMoves = self.canMove(x, y)
+        if self.probability(self.epsilon):
             return choice(availableMoves)
         else:
-            maxValue, moves = computeMax(x, y, availableMoves)
+            maxValue, moves = self.computeMax(x, y, availableMoves, Qas)
             return choice(moves)
 
-    def computeMax(self, pos):
+    def computeMax(self, x, y, pos, Qas):
         res = -9999999999999999999
         best = []
-        for i in range(len(4)):
-            current = self.Qas[pos[0]][pos[1]][i]
+        for i in range(len(pos)):
+            current = Qas[x][y][pos[i]]
             if current > res:
                 res = current
-                best = [i]
+                best = [pos[i]]
             elif current == res:
-                best.append(i)
+                best.append(pos[i])
         return res, best
 
     def getNextPos(self, x, y, move):
-        return [pos[0] + self.moves[move][0], pos[1] + self.moves[move][1]]
+        return [x + self.moves[move][0], y + self.moves[move][1]]
 
     def getReward(self, nextPos):
         if nextPos == self.goal:
@@ -202,17 +234,25 @@ class rsMARL:
                 res += 1
         return res*100
                 
-    def updateQ(self, reward, nextPos, currentPos, action):
-        availablePoses, availableMoves = canMove(nextPos[0], nextPos[1])
-        maxA, moves = computeMax(nextPos[0], nextPos[1], availableMoves)
-        originalQ = self.Qas[currentPos[0]][currentPos[1]][action]
-        self.Qas[currentPos[0]][currentPos[1]][action] =\
+    def updateQ(self, reward, nextPos, currentPos, action, Qas):
+        availablePoses, availableMoves = self.canMove(nextPos[0], nextPos[1])
+        maxA, moves = self.computeMax(nextPos[0], nextPos[1], availableMoves, Qas)
+        originalQ = Qas[currentPos[0]][currentPos[1]][action]
+        Qas[currentPos[0]][currentPos[1]][action] =\
             originalQ + self.alpha*(reward + self.gamma*maxA-originalQ)
 
+        return Qas
 
 
+    def checkFlags(self, pos):
+        if pos in self.flags:
+            self.goalReached[self.flags.index(pos)] = True            
 
 
 
 if __name__ == '__main__':
-    marl = rsMARL("world.txt")
+    epsilon = 0.1
+    gamma = 1
+    alpha = 0.5
+    marl = rsMARL("world.txt", epsilon, gamma, alpha)
+    marl.run()
