3312e311348574c8afee01f20f886e313b52c58d
spicard <spicard@ulb.ac.be>
2015-12-30 18:59:34 +0100

    class world

--- rsMARL.py
+++ rsMARL.py
@@ -1,13 +1,16 @@
 from random import random, choice
 import copy
+from world import World
+from worldParser import parse
 
 shapeDic = {"None":0, "Flag":1, "Solo":2, "Join":3, "Flag+Solo":4, "Flag+Join":5}
 
 class rsMARL:
-    def __init__(self, filename, epsilon, gamma, alpha, shape, lambd):
-        self.parse(filename)
+    def __init__(self, w, epsilon, gamma, alpha, shape, lambd, plans, start):
+        
+        self.world = w
 
-        self.shape=shape
+        self.shape = shape
         self.epsilon = epsilon
         self.gamma = gamma
         self.alpha = alpha
@@ -15,6 +18,13 @@ class rsMARL:
 
         self.moves = [[0, -1], [1, 0], [0, 1], [-1, 0]] #x y N E S O
         self.totalReward = 0
+
+        self.plan1Join = plans[0]
+        self.plan2Join = plans[1]
+        self.plan1Solo = plans[2]
+        self.plan2Solo = plans[3]
+
+        self.start = start
 
         """
         size = width, height
@@ -27,191 +37,66 @@ class rsMARL:
         Qas[y][x]
         eTrace[y][x]
         """
-    
-    def parse(self, filename):
-        file = open(filename)
-        content = file.readlines()
-        self.size = list(map(int, content[0].strip().replace("\n", "").split(" ")))
-        del content[0]
-        #print(self.size)
-
-        tmp = content[0].strip().strip("\n").split(", ")
-        for i in range(len(tmp)):
-            tmp[i] = tmp[i].split(" ")
-        self.goal = list(map(int, tmp[0]))
-        self.start = tmp[1:]
-        for i in range(len(self.start)):
-            self.start[i] = list(map(int, self.start[i]))
-        self.nbAgent = len(self.start)
-        #print(self.start)
-        del content[0]
-        #print(self.goal)
-
-        # Doors
-        tmp = content[0].strip().replace("\n", "").split(", ")
-
-        self.doors = [[] for i in range(len(tmp))]
-        for i in range(len(tmp)):
-            self.doors[i] = list(map(int, tmp[i].split(" "))) #Convert list str into list int
-        del content[0]
-        #print(self.doors)
-
-        # Flags
-        tmp = content[0].strip().replace("\n", "").split(", ")
-        self.flags = [[0, 0] for i in range(len(tmp))]
-        for i in range(len(tmp)):
-            self.flags[i] = list(map(int, tmp[i].split(" "))) #Convert list str into list int
-        del content[0]
-        #print(self.flags)
-
-        self.world = [[0 for i in range(int(self.size[0]))] for j in range(int(self.size[1]))]
-        for y in range(int(self.size[1])):
-            content[y] = content[y].replace("\n", "")
-            for x in range(int(self.size[0])):
-                self.world[y][x] = int(content[y][x])
-        #print(self.world)
-        del content[:13]
-        #print(content)
-        
-        size = int(content[0].strip("\n"))
-        del content[0]
-        self.flagsNames = []
-        for i in range(size):
-            self.flagsNames.append(content[0].strip("\n"))
-            del content[0]
-            
-        size = int(content[0].strip("\n"))
-        del content[0]
-        self.roomsNames = []
-        for i in range(size):
-            self.roomsNames.append(content[0].strip("\n"))
-            del content[0]
-        #print(self.flagsNames, self.roomsNames)
-            
-        size = int(content[0].strip("\n"))
-        del content[0]
-        self.plan1Join = []
-        for i in range(size):
-            self.plan1Join.append(content[0].strip("\n").split(" "))
-            del content[0]
-            
-        size = int(content[0].strip("\n"))
-        del content[0]
-        self.plan2Join = []
-        for i in range(size):
-            self.plan2Join.append(content[0].strip("\n").split(" "))
-            del content[0]
-            
-        size = int(content[0].strip("\n"))
-        del content[0]
-        self.plan1Solo = []
-        for i in range(size):
-            self.plan1Solo.append(content[0].strip("\n").split(" "))
-            del content[0]
-            
-        size = int(content[0].strip("\n"))
-        del content[0]
-        self.plan2Solo = []
-        for i in range(size):
-            self.plan2Solo.append(content[0].strip("\n").split(" "))
-            del content[0]
-            
-        self.plan1Join = self.handlePlan(self.plan1Join)
-        self.plan2Join = self.handlePlan(self.plan2Join)
-        self.plan1Solo = self.handlePlan(self.plan1Solo)
-        self.plan2Solo = self.handlePlan(self.plan2Solo)
-
-    def handlePlan(self, plan):
-        for i in range(len(plan)):
-            flags = []
-            room = plan[i][0]
-            room = self.roomsNames.index(room)
-            if len(plan[i]) > 1:
-                flags = plan[i][1:]
-                for j in range(len(flags)):
-                    flags[j] = self.flagsNames.index(flags[j])
-            plan[i] = [room] + flags
-        return plan
-
-    def canMove(self, x, y, agent):
-        availablePoses = []
-        availableMoves = []
-        potentialPoses = [[x+self.moves[i][0], y+self.moves[i][1]] for i in range(4)]
-        potentialMoves = range(4)
-
-        for i in range(len(potentialPoses)):
-            if potentialPoses[i][0] >= 0 and potentialPoses[i][1] >= 0 and potentialPoses[i][0] < self.size[0] and potentialPoses[i][1] < self.size[1]:
-
-                if self.isInSameRoom([x, y], potentialPoses[i]):
-                    availableMoves.append(potentialMoves[i])
-                    availablePoses.append(potentialPoses[i])
-
-                elif self.hasDoor([x, y], potentialPoses[i]):
-                    availableMoves.append(potentialMoves[i])
-                    availablePoses.append(potentialPoses[i])
-
-        """
-        if agent == 2 and self.agent1 in availablePoses and self.agent1!=self.goal:
-            availableMoves.pop(availablePoses.index(self.agent1))
-            availablePoses.pop(availablePoses.index(self.agent1))
-        elif agent == 1 and self.agent2 in availablePoses and self.agent2!=self.goal:
-            availableMoves.pop(availablePoses.index(self.agent2))
-            availablePoses.pop(availablePoses.index(self.agent2))
-        """
-
-        return availableMoves
-
-    def hasDoor(self, coord1, coord2):
-        return (coord1+coord2) in self.doors or (coord2+coord1) in self.doors
-
-    def isInSameRoom(self, coord1, coord2):
-        return self.world[coord1[1]][coord1[0]] == self.world[coord2[1]][coord2[0]]
-
 
     def isDone(self):
-        return self.agent1 == self.goal and self.agent2 == self.goal
+        return self.world.isOnGoal(self.agent1) and self.world.isOnGoal(self.agent2)
 
 
     def reset(self):
         self.agent1 = self.start[0]
         self.agent2 = self.start[1]
-        self.goalReached = [False]*len(self.flags)
-        self.goalReached1 = [False]*len(self.flags)
-        self.goalReached2 = [False]*len(self.flags)
+        self.goalReached = [False]*len(self.world.flags)
+        self.goalReached1 = [False]*len(self.world.flags)
+        self.goalReached2 = [False]*len(self.world.flags)
         self.flagsGot1 = []
         self.flagsGot2 = []
         self.lastStep1 = 0
         self.lastStep2 = 0
+        self.firstDone = None
 
     def run(self):
-        Qas1 = [[[0, 0, 0, 0] for i in range(self.size[0])] for j in range(self.size[1])]
-        Qas2 = [[[0, 0, 0, 0] for i in range(self.size[0])] for j in range(self.size[1])]
+        Qas1 = [[[0.0, 0.0, 0.0, 0.0] for i in range(self.world.size[0])] for j in range(self.world.size[1])]
+        Qas2 = [[[0.0, 0.0, 0.0, 0.0] for i in range(self.world.size[0])] for j in range(self.world.size[1])]
         z = False
         
 
         for run in range(1000):
             self.reset()
             step = 0
-            eTrace1 = [[[0, 0, 0, 0] for i in range(self.size[0])] for j in range(self.size[1])]
-            eTrace2 = [[[0, 0, 0, 0] for i in range(self.size[0])] for j in range(self.size[1])]
+            eTrace1 = [[[0.0, 0.0, 0.0, 0.0] for i in range(self.world.size[0])] for j in range(self.world.size[1])]
+            eTrace2 = [[[0.0, 0.0, 0.0, 0.0] for i in range(self.world.size[0])] for j in range(self.world.size[1])]
             sigma1 = None
             sigma2 = None
-            action1 = choice(self.canMove(self.agent1[0], self.agent1[1], 1))
-            action2 = choice(self.canMove(self.agent2[0], self.agent2[1], 2))
+            action1 = choice(self.world.canMove(self.agent1))
+            action2 = choice(self.world.canMove(self.agent2))
+            path1 = []
+            path2 = []
+            done1 = False
+            done2 = False
             while not self.isDone() :
                 step += 1
 
-                if self.agent1 != self.goal:
-                    self.agent1, Qas1, eTrace1, action1, p1 = self.runAgent(self.agent1, Qas1, eTrace1, action1, 1)
+                if not self.world.isOnGoal(self.agent1) and not done1:
+                    path1.append(self.agent1)
+                    self.agent1, Qas1, eTrace1, action1, p1, done1 = self.runAgent(self.agent1, Qas1, eTrace1, action1, 1)
 
-                if self.agent2 != self.goal:
-                    self.agent2, Qas2, eTrace2, action2, p2 = self.runAgent(self.agent2, Qas2, eTrace2, action2, 2)
+                if not self.world.isOnGoal(self.agent2) and not done2:
+                    path2.append(self.agent2)
+                    self.agent2, Qas2, eTrace2, action2, p2, done2 = self.runAgent(self.agent2, Qas2, eTrace2, action2, 2)
+
+                if done1 and done2:
+                    #print("okFinis")
+                    if self.firstDone == 1:
+                        self.agent1, Qas1, eTrace1, action1, p1, done1 = self.finishEpisode(self.agent1, Qas1, eTrace1, action1, 1)
+                    else:
+                        self.agent2, Qas2, eTrace2, action2, p2, done2 = self.finishEpisode(self.agent2, Qas2, eTrace2, action2, 2)
+
 
                 if step%100 == 0:
                     if self.shape == "Solo" or self.shape == "Join":
-                        print(step, self.agent1!= self.goal, self.agent2 != self.goal, self.getStepPlan(1, self.agent1, self.shape), self.getStepPlan(2, self.agent2, self.shape))
+                        print(step, self.world.isOnGoal(self.agent1), self.world.isOnGoal(self.agent2), self.getStepPlan(1, self.agent1, self.shape), self.getStepPlan(2, self.agent2, self.shape))
                     else:
-                        print(step, self.agent1!= self.goal, self.agent2 != self.goal)
+                        print(step, done1, done2)
                 #print (self.agent2)
                 if step > 10000 and not z:
                     a = input("What to do")
@@ -220,14 +105,14 @@ class rsMARL:
                         pass
                     else:
                         print("ok")
-                        if self.agent1 != self.goal:
-                            print(self.agent1, self.canMove(self.agent1[0], self.agent1[1], 1), Qas1[self.agent1[1]][self.agent1[0]], action1, p1, self.testFlags([0,0],1))
+                        if not self.world.isOnGoal(self.agent1):
+                            print(self.agent1, self.world.canMove(self.agent1), Qas1[self.agent1[1]][self.agent1[0]], action1, p1, self.testFlags([0,0],1))
                             for i in range(len(Qas1)):
                                 for j in range(len(Qas1[i])):
                                     print(list(map(int, Qas1[i][j])), end= " ")
                                 print("")
-                        if self.agent2 != self.goal:
-                            print(self.agent2, self.canMove(self.agent2[0], self.agent2[1], 2), Qas2[self.agent2[1]][self.agent2[0]], action2, p2, self.testFlags([0,0],2))
+                        if not self.world.isOnGoal(self.agent2):
+                            print(self.agent2, self.world.canMove(self.agent2), Qas2[self.agent2[1]][self.agent2[0]], action2, p2, self.testFlags([0,0],2))
                             for i in range(len(Qas2)):
                                 for j in range(len(Qas2[i])):
                                     print(list(map(int, Qas2[i][j])), end= " ")
@@ -235,51 +120,80 @@ class rsMARL:
 
             self.totalReward += self.getFinalReward()
             print (run, step, self.goalReached.count(True), int(self.totalReward/(run+1)))
+        print(path2)
+        print(path1)
 
     def runAgent(self, pos, Qas, eTrace, action, agent):
         #action = self.chooseAction(pos[0], pos[1], Qas)
+        done = False
         nextPos = self.getNextPos(pos[0], pos[1], action)
+        if self.world.isOnGoal(nextPos):
+            if self.firstDone == None:
+                self.firstDone = agent
+                return pos, Qas, eTrace, action, "", True
+            else:
+                done = True
         reward = self.getReward(nextPos)
+        #reward = self.testFlags(pos, agent)*100
+        #if not self.world.isOnGoal(pos):
+        #    reward = 0
 
         sigma, nextAction, p = self.getSigma(reward, nextPos, pos, action, Qas, agent)
         eTrace[pos[1]][pos[0]][action] = 1
         eTrace, Qas = self.updateEligibilityTrace(eTrace, sigma, Qas)
 
         self.checkFlags(nextPos, agent)
-        return nextPos, Qas, eTrace, nextAction, p
+        return nextPos, Qas, eTrace, nextAction, p, done
+
+    def finishEpisode(self, pos, Qas, eTrace, action, agent):
+        #action = self.chooseAction(pos[0], pos[1], Qas)
+        nextPos = self.getNextPos(pos[0], pos[1], action)
+        reward = self.getReward(nextPos)
+        #reward = self.testFlags(pos, agent)*100
+        #if not self.world.isOnGoal(pos):
+        #    reward = 0
+
+        sigma, nextAction, p = self.getSigma(reward, nextPos, pos, action, Qas, agent)
+        eTrace[pos[1]][pos[0]][action] = 1
+        eTrace, Qas = self.updateEligibilityTrace(eTrace, sigma, Qas)
+
+        self.checkFlags(nextPos, agent)
+        return nextPos, Qas, eTrace, nextAction, p, True
 
 
     def probability(self, p):
         return random() < p
 
-    def chooseAction(self, x, y, Qas, agent):
-        availableMoves = self.canMove(x, y, agent)
+    def chooseAction(self, pos, Qas, agent):
+        availableMoves = self.world.canMove(pos)
         if self.probability(self.epsilon):
             return choice(availableMoves), "random"
         else:
-            maxValue, moves = self.computeMax(x, y, availableMoves, Qas)
+            maxValue, moves = self.computeMax(pos, availableMoves, Qas)
             return choice(moves), "greedy"
 
-    def computeMax(self, x, y, pos, Qas):
+    def computeMax(self, pos, moves, Qas):
+        x = pos[0]
+        y = pos[1]
         res = -9999999999999999999
         best = []
-        for i in range(len(pos)):
-            current = Qas[y][x][pos[i]]
+        for i in range(len(moves)):
+            current = Qas[y][x][moves[i]]
             if current > res:
                 res = current
-                best = [pos[i]]
+                best = [moves[i]]
             elif current == res:
-                best.append(pos[i])
+                best.append(moves[i])
         return res, best
 
     def getNextPos(self, x, y, move):
         return [x + self.moves[move][0], y + self.moves[move][1]]
 
     def getReward(self, pos):
-        if pos == self.goal:
+        if self.world.isOnGoal(pos):
             return self.getFinalReward()
         else:
-            return 0
+            return -1
 
     def getFinalReward(self):
         return 100*self.goalReached.count(True)
@@ -290,11 +204,13 @@ class rsMARL:
                 for a in range(len(Qas[y][x])):
                     Qas[y][x][a] += self.alpha * sigma * eTrace[y][x][a]
                     eTrace[y][x][a] *= self.gamma * self.lambd
+                    if eTrace[y][x][a] < 0.0000000001:
+                        eTrace[y][x][a] = 0
         return eTrace, Qas
         
                 
     def getSigma(self, reward, nextPos, currentPos, action, Qas, agent):
-        nextAction, p = self.chooseAction(nextPos[0], nextPos[1], Qas, agent)
+        nextAction, p = self.chooseAction(nextPos, Qas, agent)
         originalQ = Qas[currentPos[1]][currentPos[0]][action]
         nextQ = Qas[nextPos[1]][nextPos[0]][nextAction]
         sigma = \
@@ -304,16 +220,16 @@ class rsMARL:
 
 
     def checkFlags(self, pos, agent):
-        if pos in self.flags:
-            self.goalReached[self.flags.index(pos)] = True
+        if pos in self.world.flags:
+            self.goalReached[self.world.flags.index(pos)] = True
             if agent == 1 :
-                self.goalReached1[self.flags.index(pos)] = True
-                if not self.flags.index(pos) in self.flagsGot1:
-                    self.flagsGot1.append(self.flags.index(pos))
+                self.goalReached1[self.world.flags.index(pos)] = True
+                if not self.world.flags.index(pos) in self.flagsGot1:
+                    self.flagsGot1.append(self.world.flags.index(pos))
             else:
-                self.goalReached2[self.flags.index(pos)] = True
-                if not self.flags.index(pos) in self.flagsGot2:
-                    self.flagsGot2.append(self.flags.index(pos))
+                self.goalReached2[self.world.flags.index(pos)] = True
+                if not self.world.flags.index(pos) in self.flagsGot2:
+                    self.flagsGot2.append(self.world.flags.index(pos))
 
 
     def testFlags(self, pos, agent):
@@ -321,8 +237,8 @@ class rsMARL:
             tmp = list(self.goalReached1)
         if agent == 2:
             tmp = list(self.goalReached2)
-        #if pos in self.flags:
-        #    tmp[self.flags.index(pos)] = True
+        if pos in self.world.flags:
+            tmp[self.world.flags.index(pos)] = True
         return tmp.count(True)
 
     def rewardShaping(self, currentPos, nextPos, agent):
@@ -363,7 +279,7 @@ class rsMARL:
             else:
                 plan = self.plan2Join
             lastStep = self.lastStep2
-        roomIn = self.getCurrentRoom(pos)
+        roomIn = self.world.getCurrentRoom(pos)
         flagsGot = self.getFlagsGot(agent)
         step = None
         for i in range(len(plan)):
@@ -402,12 +318,20 @@ class rsMARL:
             res = self.flagsGot2
         return res
 
-
-
 if __name__ == '__main__':
+
+    size, goal, start, doors, flags, world, flagsNames, roomsNames, plan1Join, plan2Join, plan1Solo, plan2Solo =\
+        parse("world.txt")
+
+    w = World(size, goal, doors, flags, world, flagsNames, roomsNames)
+
+    print(len(w.flags), w.flags)
+
+
     epsilon = 0.1
     gamma = 0.99
     alpha = 0.1
     lambd = 0.4
-    marl = rsMARL("world.txt", epsilon, gamma, alpha, "Join", lambd)
+    plans = (plan1Join, plan2Join, plan1Solo, plan2Solo)
+    marl = rsMARL(w, epsilon, gamma, alpha, "Flags", lambd, plans, start)
     marl.run()
---
+++ world.py
@@ -0,0 +1,88 @@
+class World:
+    def __init__(self, size, goal, doors, flags, world, flagsNames, roomsNames):
+        self.size = size
+        self.goal = goal
+        self.doors = doors
+        self.flags = flags
+        self.world = world
+        self.flagsNames = flagsNames
+        self.roomsNames = roomsNames
+
+        self.moves = [[0, -1], [1, 0], [0, 1], [-1, 0]] #x y N E S O
+
+    def canMoveAvoid(self, pos, lastAction):
+        x = pos[0]
+        y = pos[1]
+        availablePoses = []
+        availableMoves = []
+        potentialPoses = [[x+self.moves[i][0], y+self.moves[i][1]] for i in range(4)]
+        potentialMoves = range(4)
+
+        for i in range(len(potentialPoses)):
+            if potentialPoses[i][0] >= 0 and potentialPoses[i][1] >= 0 and potentialPoses[i][0] < self.size[0] and potentialPoses[i][1] < self.size[1]:
+
+                if self.isInSameRoom([x, y], potentialPoses[i]):
+                    availableMoves.append(potentialMoves[i])
+                    availablePoses.append(potentialPoses[i])
+
+                elif self.hasDoor([x, y], potentialPoses[i]):
+                    availableMoves.append(potentialMoves[i])
+                    availablePoses.append(potentialPoses[i])
+        actionToAvoid = (lastAction + 2) %4
+        if actionToAvoid in availableMoves:
+            availableMoves.pop(availableMoves.index(actionToAvoid))
+        return availableMoves
+
+    def canMove(self, pos):
+        x = pos[0]
+        y = pos[1]
+        availablePoses = []
+        availableMoves = []
+        potentialPoses = [[x+self.moves[i][0], y+self.moves[i][1]] for i in range(4)]
+        potentialMoves = range(4)
+
+        for i in range(len(potentialPoses)):
+            if potentialPoses[i][0] >= 0 and potentialPoses[i][1] >= 0 and potentialPoses[i][0] < self.size[0] and potentialPoses[i][1] < self.size[1]:
+
+                if self.isInSameRoom([x, y], potentialPoses[i]):
+                    availableMoves.append(potentialMoves[i])
+                    availablePoses.append(potentialPoses[i])
+
+                elif self.hasDoor([x, y], potentialPoses[i]):
+                    availableMoves.append(potentialMoves[i])
+                    availablePoses.append(potentialPoses[i])
+
+        """
+        if agent == 2 and self.agent1 in availablePoses and self.agent1!=self.goal:
+            availableMoves.pop(availablePoses.index(self.agent1))
+            availablePoses.pop(availablePoses.index(self.agent1))
+        elif agent == 1 and self.agent2 in availablePoses and self.agent2!=self.goal:
+            availableMoves.pop(availablePoses.index(self.agent2))
+            availablePoses.pop(availablePoses.index(self.agent2))
+        """
+
+        return availableMoves
+
+    def hasDoor(self, coord1, coord2):
+        return (coord1+coord2) in self.doors or (coord2+coord1) in self.doors
+
+    def isInSameRoom(self, coord1, coord2):
+        return self.world[coord1[1]][coord1[0]] == self.world[coord2[1]][coord2[0]]
+
+    def isOnGoal(self, pos):
+        return pos == self.goal
+
+    def getNextPos(self, pos, move):
+        return [pos[0] + self.moves[move][0], pos[1] + self.moves[move][1]]
+
+    def isOnFlag(self, pos):
+        if pos in self.flags:
+            return True
+        else:
+            return False
+
+    def getFlag(self, pos):
+        return self.flags.index(pos)
+
+    def getCurrentRoom(self, pos):
+        return self.world[pos[1]][pos[0]]
